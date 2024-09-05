851f6e634e11fb0820ec677657a42c08e3262656
edu <vizuete.eduardo@gmail.com>
2024-09-04 22:39:56 -0500

    Section 5: Concurrency

---
+++ section_05_concurrency/concurrency/src/main/java/concurrency/codecourse/SynchronizedCollection.java
@@ -0,0 +1,17 @@
+package concurrency.codecourse;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SynchronizedCollection {
+    public static void main(String[] args) {
+        List<String> dogTypes = new ArrayList<>();
+        dogTypes.add("German Shepherd");
+        dogTypes.add("Labrador");
+        List<String> dogTypesSyn = Collections.synchronizedList(dogTypes);
+
+        // safe to use dogTypesSyn with multiple threads...
+    }
+
+}
