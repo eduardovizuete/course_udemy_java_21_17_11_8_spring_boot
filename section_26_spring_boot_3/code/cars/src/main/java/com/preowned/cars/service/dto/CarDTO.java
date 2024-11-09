539a9394fe44869c19ad9c046502bdfd198eb354
edu <vizuete.eduardo@gmail.com>
2024-11-08 22:21:46 -0500

    Section 26: Spring boot 3 - controller layer - hateoas

--- section_26_spring_boot_3/code/cars/src/main/java/com/preowned/cars/service/dto/CarDTO.java
+++ section_26_spring_boot_3/code/cars/src/main/java/com/preowned/cars/service/dto/CarDTO.java
@@ -1,10 +1,11 @@
 package com.preowned.cars.service.dto;
 
 import lombok.Data;
+import org.springframework.hateoas.RepresentationModel;
 
 // @Data : Generates getters/setters, a constructor, toString, hashCode and equals.
 @Data
-public class CarDTO {
+public class CarDTO extends RepresentationModel<CarDTO> {
 
     private String brandName, modelName, regNo, carType;
     private int year, kms, price;
