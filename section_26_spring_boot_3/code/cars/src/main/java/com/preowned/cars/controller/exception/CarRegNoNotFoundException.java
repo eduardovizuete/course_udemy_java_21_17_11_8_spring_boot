93ad61d75ca3cf182d89d97b219ffb18c963aac1
edu <vizuete.eduardo@gmail.com>
2025-02-26 18:02:37 -0500

    Section 26(28): Spring boot 3 - update exception

--- section_26_spring_boot_3/code/cars/src/main/java/com/preowned/cars/controller/exception/CarRegNoNotFoundException.java
+++ section_26_spring_boot_3/code/cars/src/main/java/com/preowned/cars/controller/exception/CarRegNoNotFoundException.java
@@ -3,7 +3,7 @@ package com.preowned.cars.controller.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class CarRegNoNotFoundException extends RuntimeException {
 
     public CarRegNoNotFoundException(String message) {
